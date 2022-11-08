32505e7793dc00424812dfe7f7aa4f12c987acc8
Gabor Jenei <gab.jenei@gmail.com>
2022-11-08 12:28:35 +0100

    Using localStorage to store the items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ const listButtonStyle = {
 }
 
 const Home: NextPage = () => {
-  const [savedItems, setSavedItems] = useState<string>('');
-  const [items, setItems] = useState<{ id: number; text: string; checked: boolean }[]>(savedItems ? JSON.parse(savedItems) : []);
+  const [savedItems, setSavedItems] = useState(typeof window !== 'undefined' ? localStorage.getItem('items') : []);
+  const [items, setItems] = useState<{ id: number; text: string; checked: boolean }[]>([]);
   const [editItem, setEditItem] = useState<number>(-1);
   const [showChecked, setShowChecked] = useState<boolean>(false);
   const [maxId, setMaxId] = useState<number>(0);
@@ -25,12 +25,19 @@ const Home: NextPage = () => {
     themeCtx.toggleThemeHandler();
   }
 
+  const setLocalStorageItems = (items: { id: number; text: string; checked: boolean }[]) => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+
   const addTodo = () => {
     if (todoInput.current === null || todoInput.current.value === '') return
 
-    setItems([...items, { id: maxId, text: todoInput.current.value, checked: false }]);
+    const updatedItems = [...items, { id: maxId, text: todoInput.current.value, checked: false }];
+    setItems(updatedItems);
     setMaxId(maxId + 1);
     todoInput.current.value = '';
+
+    setLocalStorageItems(updatedItems);
   }
 
   const editTodo = () => {
@@ -52,6 +59,7 @@ const Home: NextPage = () => {
     setItems(updatedItems);
     todoInput.current.value = '';
     setEditItem(-1);
+    setLocalStorageItems(updatedItems);
   }
 
   const handleTodo = (e: React.ChangeEvent<HTMLFormElement>) => {
@@ -65,10 +73,6 @@ const Home: NextPage = () => {
   }
 
   useEffect(() => {
-    window.localStorage.setItem('todos', JSON.stringify(items));
-  }, [items]);
-
-  useEffect(() => {
     const saved = localStorage.getItem('todos');
 
     if (saved) {
@@ -76,12 +80,9 @@ const Home: NextPage = () => {
     }
 
     // TODO: Need to figure out how to save data on reload
-    // const localItems = JSON.parse(window.localStorage.getItem('todos')) || [];
+    const localItems = JSON.parse(window.localStorage.getItem('items')!);
 
-    // console.table(localItems);
-    // if (localItems !== undefined) {
-    //   setItems(localItems);
-    // };
+    setItems(localItems);
   }, [])
 
   useEffect(() => {
@@ -103,21 +104,25 @@ const Home: NextPage = () => {
       }
     })
     setItems(updatedItems);
+    setLocalStorageItems(updatedItems);
   };
 
   const toggleAll = () => {
     const updatedItems = items.map(item => ({ id: item.id, text: item.text, checked: !showChecked }));
     setItems(updatedItems);
+    setLocalStorageItems(updatedItems);
   }
 
   const removeAll = () => {
     const updatedItems = items.filter(item => item.checked !== showChecked);
     setItems(updatedItems);
+    setLocalStorageItems(updatedItems);
   }
 
   const removeItem = (id: number) => {
     const updatedItems = items.filter(item => item.id !== id);
     setItems(updatedItems);
+    setLocalStorageItems(updatedItems);
   }
 
   return (
@@ -140,12 +145,12 @@ const Home: NextPage = () => {
 
             <div className="opacity-100 dark:opacity-0 fill-slate-800 w-4" >
               <svg viewBox="0 0 17 16" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.914 0a6.874 6.874 0 0 0-1.26 3.972c0 3.875 3.213 7.017 7.178 7.017.943 0 1.843-.178 2.668-.5C15.423 13.688 12.34 16 8.704 16 4.174 16 .5 12.41.5 7.982.5 3.814 3.754.389 7.914 0z" fill-rule="evenodd"></path>
+                <path d="M7.914 0a6.874 6.874 0 0 0-1.26 3.972c0 3.875 3.213 7.017 7.178 7.017.943 0 1.843-.178 2.668-.5C15.423 13.688 12.34 16 8.704 16 4.174 16 .5 12.41.5 7.982.5 3.814 3.754.389 7.914 0z" fillRule="evenodd"></path>
               </svg>
             </div>
             <div className="opacity-0 dark:opacity-100 fill-yellow-200 w-4" >
               <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3.828 5.243L2.343 3.757a1 1 0 0 1 1.414-1.414l1.486 1.485a5.027 5.027 0 0 0-1.415 1.415zM7 3.1V1a1 1 0 1 1 2 0v2.1a5.023 5.023 0 0 0-2 0zm3.757.728l1.486-1.485a1 1 0 1 1 1.414 1.414l-1.485 1.486a5.027 5.027 0 0 0-1.415-1.415zM12.9 7H15a1 1 0 0 1 0 2h-2.1a5.023 5.023 0 0 0 0-2zm-.728 3.757l1.485 1.486a1 1 0 1 1-1.414 1.414l-1.486-1.485a5.027 5.027 0 0 0 1.415-1.415zM9 12.9V15a1 1 0 0 1-2 0v-2.1a5.023 5.023 0 0 0 2 0zm-3.757-.728l-1.486 1.485a1 1 0 0 1-1.414-1.414l1.485-1.486a5.027 5.027 0 0 0 1.415 1.415zM3.1 9H1a1 1 0 1 1 0-2h2.1a5.023 5.023 0 0 0 0 2zM8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" fill-rule="evenodd"></path>
+                <path d="M3.828 5.243L2.343 3.757a1 1 0 0 1 1.414-1.414l1.486 1.485a5.027 5.027 0 0 0-1.415 1.415zM7 3.1V1a1 1 0 1 1 2 0v2.1a5.023 5.023 0 0 0-2 0zm3.757.728l1.486-1.485a1 1 0 1 1 1.414 1.414l-1.485 1.486a5.027 5.027 0 0 0-1.415-1.415zM12.9 7H15a1 1 0 0 1 0 2h-2.1a5.023 5.023 0 0 0 0-2zm-.728 3.757l1.485 1.486a1 1 0 1 1-1.414 1.414l-1.486-1.485a5.027 5.027 0 0 0 1.415-1.415zM9 12.9V15a1 1 0 0 1-2 0v-2.1a5.023 5.023 0 0 0 2 0zm-3.757-.728l-1.486 1.485a1 1 0 0 1-1.414-1.414l1.485-1.486a5.027 5.027 0 0 0 1.415 1.415zM3.1 9H1a1 1 0 1 1 0-2h2.1a5.023 5.023 0 0 0 0 2zM8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" fillRule="evenodd"></path>
               </svg>
             </div>
           </button>
