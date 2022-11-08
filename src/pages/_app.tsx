e6509c8128b105947f5d13513e9404367437f5e0
Gabor Jenei <gab.jenei@gmail.com>
2022-11-08 11:19:28 +0100

    Added dark and light theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
+import { MyThemeContextProvider } from "../components/myThemeContext";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MyThemeContextProvider>
+      <Component {...pageProps} />
+    </MyThemeContextProvider>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { FiCircle, FiCheckCircle, FiEdit2, FiTrash2 } from "react-icons/fi";
+import MyThemeContext from "../components/myThemeContext";
 
 const listButtonStyle = {
   color: "#9333ea",
   cursor: "default",
   borderColor: "#9333ea"
-}
-
-const useLightMode = () => {
-  const [theme, setTheme] = useState<string>(typeof window !== 'undefined' ? localStorage.theme : 'light');
-  const colorTheme = theme === 'light' ? 'dark' : 'light';
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-
-    root.classList.remove(colorTheme);
-    root.classList.add(theme);
-
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', theme);
-    }
-
-  }, [theme])
-
-  return [colorTheme, setTheme];
 }
 
 const Home: NextPage = () => {
@@ -36,7 +18,12 @@ const Home: NextPage = () => {
   const [maxId, setMaxId] = useState<number>(0);
   const todoInput = useRef<HTMLInputElement>(null);
   const todoButton = useRef<HTMLButtonElement>(null);
-  const [colorTheme, setTheme] = useLightMode();
+  const themeCtx: { isDarkMode?: boolean; toggleThemeHandler: () => void } =
+    useContext(MyThemeContext);
+
+  function toggleThemeHandler(): void {
+    themeCtx.toggleThemeHandler();
+  }
 
   const addTodo = () => {
     if (todoInput.current === null || todoInput.current.value === '') return
@@ -142,98 +129,123 @@ const Home: NextPage = () => {
       </Head>
 
 
-      <main className="container mx-auto min-h-screen flex justify-center">
-        <div className="flex text-white flex-col items-center w-full sm:w-3/4 gap-4 p-5 mt-10">
-          <h2 className="text-4xl font-extrabold">What <span className="text-purple-600">ToDo</span></h2>
-          <div>
-            <form className="grid" onSubmit={handleTodo}>
-              <input className="border border-slate-600 transition-all duration-200 ease-in-out outline-none px-2 py-1 rounded bg-slate-600 hover:drop-shadow-purple-600]" autoFocus ref={todoInput} type="text" />
-              <div className="m-1"></div>
-              <button ref={todoButton}
-                className="uppercase py-1.5 font-extrabold bg-purple-600 rounded duration-300 motion-safe:hover:bg-purple-800"
-              >{
-                  editItem >= 0
-                    ? "Edit item"
-                    : "Add item"
-                }</button>
-            </form>
-          </div>
+      <main className="w-full min-h-screen bg-violet-50 dark:bg-slate-800">
 
-          <div className="flex w-full text-gray-500 mt-6 w-full">
-            <button
-              onClick={() => setShowChecked(false)}
-              className="px-4 w-full font-bold transition-all duration-500 ease-in-out hover:text-purple-600 border-b-2 pb-2 border-gray-700"
-              style={!showChecked ? listButtonStyle : {}}>
-              Active items ({items.filter(item => item.checked === false).length})
-            </button>
-            <button
-              onClick={() => setShowChecked(true)}
-              className="px-4 w-full font-bold transition-all duration-500 ease-in-out hover:text-purple-600 border-b-2 pb-2 border-gray-700 hover:border-purple-600"
-              style={showChecked ? listButtonStyle : {}}>
-              Checked items ({items.filter(item => item.checked === true).length})
-            </button>
-          </div>
+        <div className="flex justify-center">
+          <button
+            type="button"
+            className="flex mt-4 border-2 border-slate-300 dark:border-slate-700 rounded-3xl px-2 py-1.5 text-black dark:text-white rounded"
+            onClick={toggleThemeHandler}
+          >
 
-          <div className="flex flex-col gap-4 w-full justify-center border p-2 border-gray-700 rounded">
-            {
-              items && (items.filter(item => item.checked === showChecked).length === 0)
-                ?
-                <div className="text-center mt-2 p-4 text-gray-500">
-                  No items found.
-                </div>
-                : <div className="flex gap-2 mt-2 place-content-between">
-                  <button onClick={() => toggleAll()} className="flex items-center gap-2 py-0.5 px-4 text-sm text-gray-100 hover:translate-y-1 transition-all duration-300 ease-in-out">
-                    {!showChecked
-                      ? <>
-                        <FiCheckCircle />
-                        Check all
-                      </>
-                      : <>
-                        <FiCircle />
-                        Uncheck all
-                      </>
-                    }
-                  </button>
-                  <button onClick={() => removeAll()} className="flex items-center gap-2 py-0.5 px-4 text-sm text-red-600 hover:translate-y-1 transition-all duration-300 ease-in-out">
-                    <FiTrash2 />
-                    Delete all
-                  </button>
-                </div>
-
-            }
+            <div className="opacity-100 dark:opacity-0 fill-slate-800 w-4" >
+              <svg viewBox="0 0 17 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.914 0a6.874 6.874 0 0 0-1.26 3.972c0 3.875 3.213 7.017 7.178 7.017.943 0 1.843-.178 2.668-.5C15.423 13.688 12.34 16 8.704 16 4.174 16 .5 12.41.5 7.982.5 3.814 3.754.389 7.914 0z" fill-rule="evenodd"></path>
+              </svg>
+            </div>
+            <div className="opacity-0 dark:opacity-100 fill-yellow-200 w-4" >
+              <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.828 5.243L2.343 3.757a1 1 0 0 1 1.414-1.414l1.486 1.485a5.027 5.027 0 0 0-1.415 1.415zM7 3.1V1a1 1 0 1 1 2 0v2.1a5.023 5.023 0 0 0-2 0zm3.757.728l1.486-1.485a1 1 0 1 1 1.414 1.414l-1.485 1.486a5.027 5.027 0 0 0-1.415-1.415zM12.9 7H15a1 1 0 0 1 0 2h-2.1a5.023 5.023 0 0 0 0-2zm-.728 3.757l1.485 1.486a1 1 0 1 1-1.414 1.414l-1.486-1.485a5.027 5.027 0 0 0 1.415-1.415zM9 12.9V15a1 1 0 0 1-2 0v-2.1a5.023 5.023 0 0 0 2 0zm-3.757-.728l-1.486 1.485a1 1 0 0 1-1.414-1.414l1.485-1.486a5.027 5.027 0 0 0 1.415 1.415zM3.1 9H1a1 1 0 1 1 0-2h2.1a5.023 5.023 0 0 0 0 2zM8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" fill-rule="evenodd"></path>
+              </svg>
+            </div>
+          </button>
+        </div>
 
 
-            <div className="flex gap-2 flex-col w-full">
+        <div className="container mx-auto min-h-screen flex justify-center">
+          <div className="flex  text-gray-700 dark:text-white flex-col items-center w-full lg:w-3/4 gap-4 p-5 mt-10">
+            <h2 className="text-4xl font-extrabold">What <span className="text-purple-600">ToDo</span></h2>
+            <div>
+              <form className="grid" onSubmit={handleTodo}>
+                <input className="border dark:border-slate-600 transition-all duration-200 ease-in-out outline-none px-2 py-1 rounded dark:bg-slate-600 hover:drop-shadow-purple-600]" autoFocus ref={todoInput} type="text" />
+                <div className="m-1"></div>
+                <button ref={todoButton}
+                  className="uppercase py-1.5 font-extrabold text-white bg-purple-600 rounded duration-300 motion-safe:hover:bg-purple-800"
+                >{
+                    editItem >= 0
+                      ? "Edit item"
+                      : "Add item"
+                  }</button>
+              </form>
+            </div>
+
+            <div className="flex w-full text-gray-400 dark:text-gray-500 mt-6 w-full">
+              <button
+                onClick={() => setShowChecked(false)}
+                className="px-4 w-full font-bold transition-all duration-500 ease-in-out hover:text-purple-600 border-b-2 pb-2 border-gray-700"
+                style={!showChecked ? listButtonStyle : {}}>
+                Active items ({items.filter(item => item.checked === false).length})
+              </button>
+              <button
+                onClick={() => setShowChecked(true)}
+                className="px-4 w-full font-bold transition-all duration-500 ease-in-out hover:text-purple-600 border-b-2 pb-2 dark:border-gray-700 hover:border-purple-600"
+                style={showChecked ? listButtonStyle : {}}>
+                Checked items ({items.filter(item => item.checked === true).length})
+              </button>
+            </div>
+
+            <div className="flex flex-col gap-4 w-full justify-center border p-2 border-gray-200 dark:border-gray-700 rounded">
               {
-                items?.filter(item => item.checked === showChecked).map(item => (
-                  <div key={item.id} className="border border-slate-700 rounded p-2 justify-between flex items-start relative item-hover transition-all duration-100 ease-in-out"
-                    style={{ textDecorationLine: item.checked ? "line-through" : 'none' }}>
-                    <div className="flex gap-3">
-                      <button onClick={() => toggleCheck(item.id)} className="text-md hover:text-purple-400 px-2 self-start pt-1">
-                        {item.checked
-                          ? <FiCheckCircle />
-                          : <FiCircle />
-                        }
-                      </button>
-                      <p className="first-letter:uppercase break-word">{item.text}</p>
-                    </div>
-                    <div className="flex sm:gap-3 self-start pt-1">
-                      <button onClick={() => setEditItem(item.id)} className="text-sm hover:text-purple-400 px-2">
-                        <FiEdit2 />
-                      </button>
-                      <button onClick={() => removeItem(item.id)} className="text-sm hover:text-purple-400 px-2">
-                        <FiTrash2 />
-                      </button>
-
-                    </div>
-
-
+                items && (items.filter(item => item.checked === showChecked).length === 0)
+                  ?
+                  <div className="text-center mt-2 p-4 text-gray-500">
+                    No items found.
                   </div>
-                ))
+                  : <div className="flex gap-2 mt-2 place-content-between">
+                    <button onClick={() => toggleAll()} className="flex items-center gap-2 py-0.5 px-4 text-sm text-gray-800 dark:text-gray-100 hover:translate-y-1 transition-all duration-300 ease-in-out">
+                      {!showChecked
+                        ? <>
+                          <FiCheckCircle />
+                          Check all
+                        </>
+                        : <>
+                          <FiCircle />
+                          Uncheck all
+                        </>
+                      }
+                    </button>
+                    <button onClick={() => removeAll()} className="flex items-center gap-2 py-0.5 px-4 text-sm text-red-600 hover:translate-y-1 transition-all duration-300 ease-in-out">
+                      <FiTrash2 />
+                      Delete all
+                    </button>
+                  </div>
+
               }
+
+
+              <div className="flex gap-2 flex-col w-full">
+                {
+                  items?.filter(item => item.checked === showChecked).map(item => (
+                    <div key={item.id} className="border border-gray-200 dark:border-gray-700 rounded p-2 justify-between flex items-start relative item-hover transition-all duration-100 ease-in-out"
+                      style={{ textDecorationLine: item.checked ? "line-through" : 'none' }}>
+                      <div className="flex gap-3">
+                        <button onClick={() => toggleCheck(item.id)} className="text-md hover:text-purple-400 px-2 self-start pt-1">
+                          {item.checked
+                            ? <FiCheckCircle />
+                            : <FiCircle />
+                          }
+                        </button>
+                        <p className="first-letter:uppercase break-word">{item.text}</p>
+                      </div>
+                      <div className="flex sm:gap-3 self-start pt-1">
+                        <button onClick={() => setEditItem(item.id)} className="text-sm hover:text-purple-400 px-2">
+                          <FiEdit2 />
+                        </button>
+                        <button onClick={() => removeItem(item.id)} className="text-sm hover:text-purple-400 px-2">
+                          <FiTrash2 />
+                        </button>
+
+                      </div>
+
+
+                    </div>
+                  ))
+                }
+              </div>
             </div>
           </div>
         </div>
+
       </main>
     </>
   );
